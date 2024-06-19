f2507c21fb4a87f97219afc21731de3fe352dfab
MD Ibrahim <shunibrahim@gmail.com>
2024-06-19 17:53:39 +0600

    session_status_updated

--- profile.php
+++ profile.php
@@ -59,149 +59,15 @@ require "db_connection.php"
         </div>
     </header>
     <h1 class="site-title">IShaan Gaming House</h1>
-    <div class="main-body-content">
-        <section class="pc">
-            <!-- pc blocs -->
-            <div class="top-pc-list">
-                <div class="pc-items">
-                    <div class="table-title">
-                        <h2>Computer Informations</h2>
-                        <a href="add-computer.php">
-                            <button class="add-item">
-                                Add Computer
-                            </button>
-                        </a>
-                    </div>
-                    <table border="0">
-                        <tr>
-                            <th>
-                                Batch ID
-                            </th>
-                            <th>
-                                PC Name
-                            </th>
-                            <th>
-                                PC Type
-                            </th>
-                            <th>
-                                PC Quality
-                            </th>
-                            <th>
-                                PC Status
-                            </th>
-                            <th colspan="2">
-                                Action
-                            </th>
-                        </tr>
-
-                        <?php
-                        $result = mysqli_query($db, "SELECT * FROM inv_pcs");
-                        while ($row = mysqli_fetch_array($result)) :
-                        ?>
-                            <tr>
-                                <td>
-                                    B0PC<?php echo $row["id"]; ?>
-                                </td>
-                                <td>
-                                    <?php echo $row["pcName"]; ?>
-                                </td>
-                                <td>
-                                    <?php echo $row["pcType"]; ?>
-                                </td>
-                                <td>
-                                    <?php echo $row["pcQuality"]; ?>
-                                </td>
-                                <td>
-                                    <?php echo $row["pcStatus"]; ?>
-                                </td>
-                                <td>
-                                    <a href="update-computer.php?id=<?php echo $row["id"]; ?> ">
-                                        <span class="material-symbols-outlined action-icons">
-                                            edit
-                                        </span>
-                                    </a>
-                                </td>
-                                <td>
-                                    <a href="delete-computer.php?id=<?php echo $row["id"]; ?> ">
-                                        <span class="material-symbols-outlined action-icons">
-                                            delete
-                                        </span>
-                                    </a>
-                                </td>
-                            </tr>
-                        <?php
-                        endwhile;
-                        ?>
-                    </table>
-                </div>
-            </div>
-        </section>
+    <div class="logout-body-content">
         <section>
-            <div class="customers">
-                <div class="top-10-customers">
-                    <div class="table-title">
-                        <h2>Customer Informations</h2>
-                        <a href="add-customer.php">
-                            <button class="add-item">
-                                Add Customer
-                            </button>
-                        </a>
-                    </div>
-                    <table border="0">
-                        <tr>
-                            <th>
-                                Customer ID
-                            </th>
-                            <th>
-                                Customer Name
-                            </th>
-                            <th>
-                                Customer Type
-                            </th>
-                            <th>
-                                Customer Status
-                            </th>
-                            <th colspan="2">
-                                Action
-                            </th>
-                        </tr>
-                        <?php
-                        $result = mysqli_query($db, "SELECT * FROM inv_customers");
-                        while ($row = mysqli_fetch_array($result)) :
-                        ?>
-                            <tr>
-                                <td>
-                                    L0C<?php echo $row["id"]; ?>
-                                </td>
-                                <td>
-                                    <?php echo $row["customerName"]; ?>
-                                </td>
-                                <td>
-                                    <?php echo $row["customerType"]; ?>
-                                </td>
-                                <td>
-                                    <?php echo $row["customerStatus"]; ?>
-                                </td>
-                                <td>
-                                    <a href="update-customer.php?id=<?php echo $row["id"]; ?>">
-                                        <span class="material-symbols-outlined action-icons">
-                                            edit
-                                        </span>
-                                    </a>
-                                </td>
-                                <td>
-                                    <a href="delete-customer.php?id=<?php echo $row["id"]; ?>">
-                                        <span class="material-symbols-outlined action-icons">
-                                            delete
-                                        </span>
-                                    </a>
-                                </td>
-                            </tr>
-                        <?php
-                        endwhile;
-                        ?>
-                    </table>
-                </div>
+            <div class="logout-title">
+                <h2>Temp Logout Here</h2>
+                <a href="logout.php">
+                    <button class="add-item">
+                        Logout
+                    </button>
+                </a>
             </div>
         </section>
     </div>
